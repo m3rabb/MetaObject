dfe8b411b6a37e2b0b1c8155f4d465204f514679
Maurice Rabb <github@mauricerabb.com>
2015-03-31 14:14:01 -0500

    Last changes to equality before move to EQMs

---
+++ docs/equality.md
@@ -0,0 +1,13 @@
+In programming, it's crucial to be able to tell whether or not two things are equivalent.  However determining whether two entities are _the same_ or not can be surprisingly challenging.  JavaScript does have two equality operators: ```===``` and ```==```.  The 
+
+Given its fundamental importance, one might be surprised that JavaScript doesn't have a built in equality method, i.e. ```isEqual()```  
+
+
+This article will discuss the challenges in implementing such a function
+
+JavaScript does ha
+
+Equality is a surprisingly tricky concept in programming.  
+
+
+JavaScript
--- scripts/top/equality.js
+++ scripts/top/equality.js
@@ -1,220 +1,225 @@
- // simpleEqulity
- // shallowEquality
- // valueEquality
- // structuralEquality
+// simpleEqulity
+// shallowEquality
+// valueEquality
+// structuralEquality
 
 Top.Extend(function () {
   var Comparator;
 
   this.$Thing.Extend(function () {
-    this.AddIMethod(function IsEqual(objB, comparator_) {
-      if (!objB instanceof Thing) {                    return false; }
-      // if (!Top.IsTopObject(objB)) {                    return false; }
-      // if (objB == null || !objB._IsEqual_) {           return false; }
-      var oidA = this.__oid;
-      var oidB = objB.__oid;
-      if (oidA === oidB) {                             return true;  }
+   this.AddIMethod(function IsEqualType(objB, comparator_) {
+     if (!objB instanceof _Thing) { return false; }
+     return this.__$type === objB.Type();
+   });
 
-      var comparator = comparator_ || Comparator.New();
-      if ( comparator.AlreadyCompared(oidA, oidB)) {   return true;  }
-      if (!comparator.HaveEqualPaths(oidA, oidB))  {   return false; }
+   this.AddIMethod(function IsEqual(objB, comparator_) {
+     if (!objB instanceof _Thing) {                   return false; }
+     // if (!this.IsEqualType(objB)) {                   return false; }
+     // if (objB == null || !objB._IsEqual_) {           return false; }
+     var oidA = this.__$oid;
+     var oidB = objB.__$oid;
+     if (oidA === oidB) {                             return true;  }
 
-      return objB._IsEqual_(this, comparator);
-    });
+     var comparator = comparator_ || Comparator.New();
+     if ( comparator.AlreadyCompared(oidA, oidB)) {   return true;  }
+     if (!comparator.HaveEqualPaths(oidA, oidB))  {   return false; }
 
-    this.AddIMethod(function _IsEqual_(_objA, comparator) {
-      if (this.IsImmutable() !== _objA.IsImmutable())  { return false; }
-      if (!this._HasEqualAncestry_(_objA, comparator)) { return false; }
-      return this._HasEqualProperties_(_objA, comparator);
-    });
+     return objB._IsEqual_(this, comparator);
+   });
 
-    this.AddIMethod(function _HasEqualAncestry_(_objA, comparator) {
-      if (_objA.Type() !== this.Type()) { return false; }
-    });
+   this.AddIMethod(function _IsEqual_(_objA, comparator) {
+     if (this.IsImmutable() !== _objA.IsImmutable())  { return false; }
+     if (!this._HasEqualAncestry_(_objA, comparator)) { return false; }
+     return this._HasEqualProperties_(_objA, comparator);
+   });
 
-    this.AddIMethod(function _HasEqualProperties_(_objA, comparator) {
-      var _objB, namesA, namesB, index, name;
+   this.AddIMethod(function _HasEqualAncestry_(_objB, comparator_) {
+     return this.__$type === _objB.__$type;
+   });
 
-      _objB = this;
-      namesA = _objA._knownProperties;
-      namesB = _objB._knownProperties;
-      if (namesA === namesB) {
-        if (namesA) {
-          index = namesA.length;
-          while (index--) {
-            name = namesA[index];
-            if (!comparator.AreEqual(_objA[name], _objB[name])) {
-              return false;
-            }
-          }
-          return true;
-        }
-        namesA = PropertiesOf(_objA);
-        index = namesA.length;
-        if (index === 0)                                    { return false; }
-        namesA.sort();
-        namesB = PropertiesOf(_objB).sort();
-      }
-      else if (namesA && namesB) {
-        index = namesA.length;
-      }
-      else                                                  { return false; }
+   this.AddIMethod(function _HasEqualProperties_(_objA, comparator) {
+     var _objB, namesA, namesB, index, name;
 
-      if (index !== namesB.length)                          { return false; }
-      while (index--) {
-        name = namesA[index];
-        if (name !== namesB[index])                         { return false; }
-        if (!comparator.AreEqual(_objA[name], _objB[name])) { return false; }
-      }
-      return true;
-    });
+     _objB = this;
+     namesA = _objA._knownProperties;
+     namesB = _objB._knownProperties;
+     if (namesA === namesB) {
+       if (namesA) {
+         index = namesA.length;
+         while (index--) {
+           name = namesA[index];
+           if (!comparator.AreEqual(_objA[name], _objB[name])) { return false; }
+         }
+         return true;
+       }
+       namesA = PropertiesOf(_objA);
+       index = namesA.length;
+       if (index === 0)                                    { return false; }
+       namesA.sort();
+       namesB = PropertiesOf(_objB).sort();
+     }
+     else if (namesA && namesB) {
+       index = namesA.length;
+     }
+     else                                                  { return false; }
+
+     if (index !== namesB.length)                          { return false; }
+     while (index--) {
+       name = namesA[index];
+       if (name !== namesB[index])                         { return false; }
+       if (!comparator.AreEqual(_objA[name], _objB[name])) { return false; }
+     }
+     return true;
+   });
   });
 
   function AreEqual(a, b) {
-    var isImmutable, comparator, oidA, oidB;
+   var measure, isImmutable, comparator, oidA, oidB;
 
-    if (a === b)   {                                      return true;   }
-    if (a == null) {                                      return a == b; }
-    if (a.IsEqual) {                          return a.IsEqual(b, this); }
-    if (typeof a !== "object" || typeof b !== "object") { return false;  }
+   if (a === b)   {                                      return true;   }
+   if (a == null) {                                      return a == b; }
 
-    isImmutable = IsImmutable(a);
-    if ((isImmutable) !== IsImmutable(b)) {               return false;  }
+   measure = this ? this.equalityMethod : "IsEqual";
+   if (a[measure]) {                          return a[measure](b, this); }
+   if (typeof a !== "object" || typeof b !== "object") { return false;  }
 
-    comparator = this || Comparator.New();
-    oidA = a.__oid || comparator._GetOid(a, isImmutable);
-    oidB = b.__oid || comparator._GetOid(b, isImmutable);
-    // if (oidA === oidB) {                               return true;   }
+   isImmutable = IsImmutable(a);
+   if ((isImmutable) !== IsImmutable(b)) {               return false;  }
 
-    if ( comparator.AlreadyCompared(oidA, oidB)) {        return true;   }
-    if (!comparator.HaveEqualPaths(oidA, oidB))  {        return false;  }
+   comparator = this || Comparator.New();
+   oidA = a.__$oid || comparator._GetOid(a, isImmutable);
+   oidB = b.__$oid || comparator._GetOid(b, isImmutable);
+   // if (oidA === oidB) {                               return true;   }
 
-    if (!comparator.HaveEqualAncestry(a, b))     {        return false;  }
-    return comparator.HaveEqualProperties(a, b);
+   if ( comparator.AlreadyCompared(oidA, oidB)) {        return true;   }
+   if (!comparator.HaveEqualPaths(oidA, oidB))  {        return false;  }
+
+   if (!comparator.HaveEqualAncestry(a, b))     {        return false;  }
+   return comparator.HaveEqualProperties(a, b);
   }
 
   Comparator = this.SetType("Comparator", function () {
-    this.AddIMethod(AreEqual);
+   this.AddIMethod(AreEqual);
 
-    this.AddIMethod(function _Init() {
-      this._immutables = [];
-      // this._modified = [];
-      this._groupings = NewStash();
-      this._visitedA = NewStash();
-      this._visitedB = NewStash();
-      this._pathCount = 0;
-    });
+   this.AddIMethod(function _Init() {
+     this._immutables = [];
+     // this._modified = [];
+     this._groupings = NewStash();
+     this._visitedA = NewStash();
+     this._visitedB = NewStash();
+     this._pathCount = 0;
+   });
 
-    this.AddIMethod(function _GetOid(target, isImmutable) {
-      var immutables, index, count;
+   this.AddIMethod(function _GetOid(target, isImmutable) {
+     var immutables, index, count;
 
-      if (isImmutable) {
-        immutables = this._immutables;
-        index = count = immutables.length;
-        while (index--) {
-          if (immutables[index] === target) { return -1 - index; }
-        }
-        immutables[count] = target;
-        return count;
-      }
-      // this._modified.push(target);
-      return (target.__oid = NewUniqueId(target.constructor.name));
-    });
+     if (isImmutable) {
+       immutables = this._immutables;
+       index = count = immutables.length;
+       while (index--) {
+         if (immutables[index] === target) { return -1 - index; }
+       }
+       immutables[count] = target;
+       return count;
+     }
+     // this._modified.push(target);
+     return (target.__$oid = NewUniqueId(target.constructor.name));
+   });
 
-    this.AddIMethod(function AlreadyCompared(oidA, oidB) {
-      var groupings = this._groupings;
-      var groupingA = groupings[oidA];
-      var groupingB = groupings[oidB];
+   this.AddIMethod(function AlreadyCompared(oidA, oidB) {
+     var groupings = this._groupings;
+     var groupingA = groupings[oidA];
+     var groupingB = groupings[oidB];
 
-      if (groupingA) {
-        if (groupingB) {
-          if (groupingA === groupingB) { return true; }
-          this._MergeGroupings(groupingA, groupingB);
-        } else {
-          groupingA.push(oidB);
-          groupings[oidB] = groupingA;
-        }
-      } else {
-        if (groupingB) {
-          groupingB.push(oidA);
-          groupings[oidA] = groupingB;
-        } else {
-          groupings[oidA] = groupings[oidB] = [oidA, oidB];
-        }
-      }
-      return false;
-    });
+     if (groupingA) {
+       if (groupingB) {
+         if (groupingA === groupingB) { return true; }
+         this._MergeGroupings(groupingA, groupingB);
+       } else {
+         groupingA.push(oidB);
+         groupings[oidB] = groupingA;
+       }
+     } else {
+       if (groupingB) {
+         groupingB.push(oidA);
+         groupings[oidA] = groupingB;
+       } else {
+         groupings[oidA] = groupings[oidB] = [oidA, oidB];
+       }
+     }
+     return false;
+   });
 
-    this.AddIMethod(function _MergeGroupings(groupingA, groupingB) {
-      var groupings = this._groupings;
-      var countA = groupingA.length;
-      var countB = groupingB.length;
-      var index, source, destination, oid;
+   this.AddIMethod(function _MergeGroupings(groupingA, groupingB) {
+     var groupings = this._groupings;
+     var countA = groupingA.length;
+     var countB = groupingB.length;
+     var index, source, destination, oid;
 
-      if (countA >= countB) {
-        index = countB;
-        source = groupingB;
-        destination = groupingA;
-      } else {
-        index = countA;
-        source = groupingA;
-        destination = groupingB;
-      }
-      Array_push.apply(destination, source);
-      while (index--) {
-        oid = source[index];
-        groupings[oid] = destination;
-      }
-    });
+     if (countA >= countB) {
+       index = countB;
+       source = groupingB;
+       destination = groupingA;
+     } else {
+       index = countA;
+       source = groupingA;
+       destination = groupingB;
+     }
+     Array_push.apply(destination, source);
+     while (index--) {
+       oid = source[index];
+       groupings[oid] = destination;
+     }
+   });
 
-    this.AddIMethod(function HaveEqualPaths(oidA, oidB) {
-      var visitedA = this._visitedA;
-      var visitedB = this._visitedB;
-      var indexA = visitedA[oidA];
-      var indexB = visitedB[oidB];
+   this.AddIMethod(function HaveEqualPaths(oidA, oidB) {
+     var visitedA = this._visitedA;
+     var visitedB = this._visitedB;
+     var indexA = visitedA[oidA];
+     var indexB = visitedB[oidB];
 
-      if (indexA !== indexB) { return false; }
-      if (indexA === undefined) {
-        visitedA[oidA] = visitedB[oidB] = this._pathCount++;
-      }
-      return true;
-    });
+     if (indexA !== indexB) { return false; }
+     if (indexA === undefined) {
+       visitedA[oidA] = visitedB[oidB] = this._pathCount++;
+     }
+     return true;
+   });
 
-    this.AddIMethod(function HaveEqualAncestry(objA, objB) {
-      return this.AreEqual(RootOf(objA), RootOf(objB));
-    });
+   this.AddIMethod(function HaveEqualAncestry(objA, objB) {
+     return this.AreEqual(RootOf(objA), RootOf(objB));
+   });
 
-    this.AddIMethod(function HaveEqualProperties(objA, objB) {
-      var namesA, namesB, index, name;
+   this.AddIMethod(function HaveEqualProperties(objA, objB) {
+     var namesA, namesB, index, name;
 
-      namesA = PropertiesOf(objA);
-      index = namesA.length;
-      if (index === 0)                                  { return false; }
-      namesB = PropertiesOf(objB);
-      if (index !== namesB.length)                      { return false; }
-      while (index--) {
-        name = namesA[index];
-        if (name !== namesB[index])                     { return false; }
-        if (!this.AreEqual(objA[name], objB[name]))     { return false; }
-      }
-      return true;
-    });
+     namesA = PropertiesOf(objA);
+     index = namesA.length;
+     if (index === 0)                                  { return false; }
+     namesB = PropertiesOf(objB);
+     if (index !== namesB.length)                      { return false; }
+     while (index--) {
+       name = namesA[index];
+       if (name !== namesB[index])                     { return false; }
+       if (!this.AreEqual(objA[name], objB[name]))     { return false; }
+     }
+     return true;
+   });
 
-    // this.AddIMethod(function Finalize() {
-    //   var modified = this._modified;
-    //   var index = modified.length
-    //   while (index--) {
-    //     delete modified[index].__oid;
-    //   }
-    // });
+   // this.AddIMethod(function Finalize() {
+   //   var modified = this._modified;
+   //   var index = modified.length
+   //   while (index--) {
+   //     delete modified[index].__$oid;
+   //   }
+   // });
   });
 
   Top.AddMethod(function AreEqual(a, b) {
-    return AreEqual.call(null, a, b);
+   return AreEqual.call(null, a, b);
   });
 
   Top.AddMethod(function IsTopObject(target) {
-    return (target instanceof _Implementation);
+   return (target instanceof _Top);
   });
 
 });
@@ -224,17 +229,17 @@ Top.Extend(function () {
 
 
 
-  // function ShallowCompare(a, b, namesA, namesB) {
-  //   var source, target, selectors, index, selector;
-  //   if (RootOf(a) !== RootOf(b)) { return false; }
-  //   namesA = PropertiesOf(a).sort();
-  //   namesB = PropertiesOf(b).sort();
-  //   index = namesA.length;
-  //   if (index !== namesB.length) { return false; }
-  //   while (index--) {
-  //     name = namesA[index];
-  //     if (name !== namesB[index]) { return false; }
-  //     if (a[name] !== b[name]) { return false; }
-  //   }
-  //   return true;
-  // }
+ // function ShallowCompare(a, b, namesA, namesB) {
+ //   var source, target, selectors, index, selector;
+ //   if (RootOf(a) !== RootOf(b)) { return false; }
+ //   namesA = PropertiesOf(a).sort();
+ //   namesB = PropertiesOf(b).sort();
+ //   index = namesA.length;
+ //   if (index !== namesB.length) { return false; }
+ //   while (index--) {
+ //     name = namesA[index];
+ //     if (name !== namesB[index]) { return false; }
+ //     if (a[name] !== b[name]) { return false; }
+ //   }
+ //   return true;
+ // }
