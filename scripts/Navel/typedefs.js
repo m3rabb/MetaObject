a898c192f8fb14f346bb9d92cfc581b71ab2665d
Maurice Rabb <dev@mauricerabb.com>
2017-06-04 12:38:28 -0500

    Add JSDoc typedefs

---
+++ scripts/Navel/typedefs.js
@@ -0,0 +1,13 @@
+/**
+ * Any
+ * @typedef {(string|number|boolean|null|undefined|symbol)} Prim
+ */
+
+
+ /**
+  * Any
+  * @typedef {Object} BooBoo
+  * @property {boolean} hasCourage - Indicates whether the Courage component is present.
+  * @property {boolean} hasPower - Indicates whether the Power component is present.
+  * @property {boolean} hasWisdom - Indicates whether the Wisdom component is present.
+  */
