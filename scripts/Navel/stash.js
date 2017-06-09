56cca94fa34c10c926468db98e561b682a0fe3dc
Maurice Rabb <dev@mauricerabb.com>
2017-06-09 17:41:35 -0500

    Add stash.js

---
+++ scripts/Navel/stash.js
@@ -0,0 +1,24 @@
+function NewStash(spec_) {
+  var top, stash, selectors, next, selector, value
+
+  top   = Object.prototype
+  stash = SpawnFrom(null)
+  if (spec_) {
+    if (HandleInheritancePoisoning && spec_ instanceof Object) {
+      selectors = AllProperties(spec_)
+      index = selectors.length
+      while (next--) {
+        selector = selectors[next]
+        value    = spec_[selector]
+        if (value !== top[selector] || HasOwnProperty.call(spec_, selector)) {
+          stash[selector] = spec_[selector]
+        }
+      }
+    } else {
+      for (selector in spec_) {
+        stash[selector] = spec_[selector]
+      }
+    }
+  }
+  return stash
+}
